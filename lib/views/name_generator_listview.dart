2b2448a0d4a1494a427b28792a45968abf84e18d
Joao <joaopaka@outlook.de>
2022-07-26 21:52:39 +0200

    Added a PopUp notification Before adding Items to the List a Popup window will appear

--- lib/views/name_generator_listview.dart
+++ lib/views/name_generator_listview.dart
@@ -9,6 +9,7 @@ class NameGenListView extends StatefulWidget {
 
 class _NameGenListViewState extends State<NameGenListView> {
   static const _title = "Edit Your List";
+  bool _remindMe = true;
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +17,7 @@ class _NameGenListViewState extends State<NameGenListView> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         iconTheme: const IconThemeData(
-          color:
-              const Color.fromARGB(255, 16, 168, 228), //change your color here
+          color: Color.fromARGB(255, 16, 168, 228), //change your color here
         ),
         backgroundColor: const Color.fromARGB(255, 255, 255, 255),
         elevation: 0,
@@ -26,11 +26,79 @@ class _NameGenListViewState extends State<NameGenListView> {
           style: TextStyle(color: Color.fromARGB(75, 0, 0, 0)),
         ),
       ),
-      body: null,
-      floatingActionButton: const FloatingActionButton(
+      body: const Center(
+        child: Text('No data'),
+      ),
+      floatingActionButton: FloatingActionButton(
         heroTag: "addContent",
-        child: Icon(Icons.add),
-        onPressed: null,
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Widget remindButton = TextButton(
+            child: const Text("Remind me later"),
+            onPressed: () {
+              return;
+            },
+          );
+          // Widget cancelButton = TextButton(
+          //   child: const Text("Cancel"),
+          //   onPressed: () {
+          //     return;
+          //   },
+          // );
+          Widget launchButton = ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: Stack(
+              children: <Widget>[
+                Positioned.fill(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: <Color>[
+                          Color(0xFF0D47A1),
+                          Color(0xFF1976D2),
+                          Color(0xFF42A5F5),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(16.0),
+                    primary: Colors.white,
+                    // textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                    _remindMe = false;
+                  },
+                  child: const Text('Got it!'),
+                ),
+              ],
+            ),
+          );
+
+          // set up the AlertDialog
+          AlertDialog alert = AlertDialog(
+            title: Text("Notice"),
+            content: const Text(
+                "All the entered Data will be Stored in a Database."),
+            actions: [
+              // cancelButton,
+              remindButton,
+              launchButton,
+            ],
+          );
+
+          // show the dialog
+          if (_remindMe) {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return alert;
+              },
+            );
+          }
+        },
       ),
     );
   }
